73d9abfc22aa74a47381e951de84e1881e75c2fe
Lhing <thorntan.r@gmail.com>
2023-12-15 16:19:07 +0700

    feat: completed ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,37 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+const sendEmailToJohn = new EmailNotification(
+  "023",
+  "10.12",
+  "Hello!!",
+  "john01@hotmail.com"
+);
+sendEmailToJohn.send();
 
-class SMSNotification { }
+const sendSMSToSara = new SMSNotification(
+  "044",
+  "10.20",
+  "Good Morning😊",
+  "081-048-9876"
+);
+sendSMSToSara.send();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,34 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
+
+const sendEmailToAmmy = new EmailNotification(
+  "033",
+  "14.09",
+  "Good Afternoon",
+  "Ammy"
+);
+sendEmailToAmmy.send();
+
+const sendSMSToMay = new SMSNotification("333", "21.08", "Good Night", "May");
+sendSMSToMay.send();
